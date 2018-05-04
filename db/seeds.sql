dc5e25acf2a8c75224f929b55a3a2666258ddfea
Josh <jhint2424@gmail.com>
2018-05-04 12:49:29 -0700

    initalization

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES('Chedda', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Bison', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Juicy', false);
