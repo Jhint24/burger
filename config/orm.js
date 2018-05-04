26a33d717e6db61f38b181cee7196587744a53a7
Josh <jhint2424@gmail.com>
2018-05-04 13:06:53 -0700

    initializing files

---
+++ config/orm.js
@@ -0,0 +1,20 @@
+var connection = require("../config/connection.js");
+
+function selectAll()    {
+
+};
+
+function insertOne()    {
+
+};
+
+function updateOne()    {
+
+};
+
+
+
+
+
+
+module.exports = orm;
