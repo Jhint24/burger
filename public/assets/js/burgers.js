7107c2d07487b7bdca4b24066b2cc72a82dbaa93
Josh <jhint2424@gmail.com>
2018-05-07 16:59:54 -0700

    form added, not working

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -16,4 +16,27 @@ $(document).ready(function()   {
             location.reload();
         });
     });
-})
+
+    $("#build-burger-button").on("click", function(event)   {
+            // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+    
+
+    var newBurger = {
+      burger_name: $("#burger-name").val().trim(),
+      devoured: 0
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+    });
+});
