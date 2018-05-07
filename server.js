e5e735cd69d7bbc2cd5505b74d0e52a30a70b9d2
Josh <jhint2424@gmail.com>
2018-05-06 19:56:15 -0700

    added server logic

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
 var express = require("express");
-var Parser = require("body-parser");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burgers_controller.js");
+
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+//static content from the "public" directory
+app.use(express.static("public"));
+
+// parse application
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Set Handlebars.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+app.use(routes);
+
+// Server Start
+app.listen(PORT, function() {
+  // Log PORT
+  console.log("Server listening on: http://localhost:" + PORT);
+});
