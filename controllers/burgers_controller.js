60d11003477d3ffb57d8e2e76ebe05bfe1c713d0
Josh <jhint2424@gmail.com>
2018-05-06 22:10:13 -0700

    added controller logic

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,7 +17,7 @@ router.get("/", function(req, res)  {
 
 //post to db
 router.post("/api/burgers", function(req, res)  {
-    burger.create(["burger_name", "devoured"],
+    burger.insertOne(["burger_name", "devoured"],
     [
         req.body.name, req.body.devoured
     ], function(result)   {
@@ -27,4 +27,21 @@ router.post("/api/burgers", function(req, res)  {
 });
 
 //update a burger
+router.put("/api/burgers/:id", function(req, res)   {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
 
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function(result)  {
+        if (result.changedRows == 0)    {
+            //no rows chagned, no id exists
+            return res.status(404).end();
+        }
+        else {
+            res.statusCode(200).end();
+        }
+    });
+});
+//export for server.js
+module.exports = router;
