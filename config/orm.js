1fb81cb0bf79e0e0b742fd97c5cab986ae98ab43
Josh <jhint2424@gmail.com>
2018-05-06 20:46:44 -0700

    adding orms

--- config/orm.js
+++ config/orm.js
@@ -3,14 +3,24 @@ var connection = require("../config/connection.js");
 var orm = {
 
     selectAll: function(tableInput, cb) {
-        var queryString = "SELECT * FROM ??;";
+        var queryString = "SELECT * FROM ??";
         connection.query(queryString, [tableInput], function(err, result)   {
             if (err) throw err;
             console.log(result)
             cb(result);
         });
+    
     },
-    insertOne: function()   {}
+    insertOne: function(table, cols, vals, cb)   {
+        var queryString = "INSERT INTO ?? VALUES ?? = ?";
+        connection.query(queryString, [table, cols, vals], function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            cb(result);
+        });
+    },
+
+    updateOne: function(table, )
 }
 
 module.exports = orm;
